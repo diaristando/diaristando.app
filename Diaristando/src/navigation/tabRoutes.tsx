9f1442c55d577a25e1f8fab9db9dda217bbafd4d
João Vitor Neves <jvneves_marq@hotmail.com>
2024-07-10 14:40:56 -0300

    feat: finishing the first steps of navigation config

--- Diaristando/src/navigation/tabRoutes.tsx
+++ Diaristando/src/navigation/tabRoutes.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
 import { Inicio } from '../screens/Inicio';
 import { PageTwo } from '../screens/PageTwo';
 
@@ -6,9 +7,23 @@ const Tab = createBottomTabNavigator();
 
 export function TabRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="inicio" component={Inicio} />
-      <Tab.Screen name="page-two" component={PageTwo} />
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="inicio"
+        component={Inicio}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name="home" size={size} color={color} />,
+          tabBarLabel: 'Início',
+        }}
+      />
+      <Tab.Screen
+        name="page-two"
+        component={PageTwo}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name="book" size={size} color={color} />,
+          tabBarLabel: 'Página 2',
+        }}
+      />
     </Tab.Navigator>
   );
 }
