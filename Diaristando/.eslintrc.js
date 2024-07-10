c9e811826598ed7bb66b50e23ebc5c24a084eeef
João Vitor Neves <jvneves_marq@hotmail.com>
2024-07-10 17:09:00 -0300

    chore: add config expo in eslint

--- Diaristando/.eslintrc.js
+++ Diaristando/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['expo', 'prettier'],
+  extends: ['expo', 'prettier', 'eslint:recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
