302024bb2ce8f48f3044c36833684062400db4e5
AnthonySa <anthonysareis11@gmail.com>
2024-07-10 14:16:43 -0300

    refact: theme folders switch to project architecture pattern

--- Diaristando/App.tsx
+++ Diaristando/App.tsx
@@ -1,4 +1,4 @@
-import './src/global/styles/global.css';
+import './src/styles/global.css';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
@@ -16,9 +16,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center gap-6 bg-primaryMedium">
       <Text className="text-h1 font-bold text-secondaryLight">Diaristando Roboto Bold</Text>
-      <Text className="text-paragraph font-regular text-tertiaryDanger">
-        Diaristando Roboto Regular
-      </Text>
+      <Text className="text-h3 font-bold text-secondaryLight">Diaristando Roboto Regular</Text>
       <StatusBar style="auto" />
     </View>
   );
--- Diaristando/tailwind.config.js
+++ Diaristando/tailwind.config.js
@@ -1,4 +1,4 @@
-import { colors, fontFamily, fontSize } from './src/global/styles/theme';
+import { colors, fontFamily, fontSize } from './src/styles';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
