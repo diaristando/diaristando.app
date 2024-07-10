f55bac3812921c21b4db2de5f0b27a4bfbeeb84d
João Vitor Neves <jvneves_marq@hotmail.com>
2024-07-10 15:54:31 -0300

    fix: adjusted conflict when merging navigation config and native wind theme

--- Diaristando/src/screens/PageTwo.tsx
+++ Diaristando/src/screens/PageTwo.tsx
@@ -1,18 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 
 export function PageTwo() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Diaristando App página 2!</Text>
+    <View className="flex-1 items-center justify-center gap-6 bg-primaryMedium">
+      <Text className="text-h1 font-bold text-secondaryLight">
+        Diaristando pagina 2 Roboto Bold
+      </Text>
+      <Text className="text-h3 font-bold text-secondaryLight">
+        Diaristando pagina 2 Roboto Regular
+      </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
