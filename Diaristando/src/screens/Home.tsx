a7c8c727df1a1677d5ce779784592f2136cae604
João Vitor Neves <jvneves_marq@hotmail.com>
2024-07-17 15:45:48 -0300

    feat: create view 'welcome message' and adjusting incorrects colors

---
+++ Diaristando/src/screens/Home.tsx
@@ -0,0 +1,28 @@
+import '../styles/global.css';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Text, View } from 'react-native';
+
+export function Home() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View className="flex-1 mx-6 py-10">
+      <View className="flex gap-2 pb-3">
+        <Text className="text-h4 font-bold text-primary">Boas vindas ao Diaristando!</Text>
+        <Text className="text-small font-bold text-dark1">
+          Te ajudando a conquistar sua independência!
+        </Text>
+      </View>
+      <Separator />
+    </View>
+  );
+}
+
+const Separator = () => <View className="border-b-2 border-primaryLight rounded-full" />;
