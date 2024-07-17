470df33c088d4f09421964b675ae33cfcca48a94
Nicalo Ribeiro <nicalo63@gmail.com>
2024-07-17 10:00:48 -0300

    feat: translator config created

---
+++ Diaristando/config/translator.ts
@@ -0,0 +1,23 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import ptbR from '../assets/locale/ptBr.json';
+
+export const supportedLanguages = {
+  ptBr: 'Português',
+};
+
+i18next.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  resources: {
+    ptBr: {
+      translation: ptbR,
+    },
+  },
+  lng: 'ptBr',
+  fallbackLng: 'ptBr',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18next;
