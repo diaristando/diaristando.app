3761f58cf31004448f586e60e3e1040a6be52aaf
João Vitor Neves <jvneves_marq@hotmail.com>
2024-07-23 19:07:19 -0300

    feat: created section 'como-funciona' with four steps

--- Diaristando/src/screens/Home.tsx
+++ Diaristando/src/screens/Home.tsx
@@ -16,23 +16,30 @@ export function Home() {
       </View>
       <Separator />
       <View className="flex gap-2 py-3">
-        <Text className="text-base font-bold text-dark1">{t('h2-como-funciona')}</Text>
-        <Text className="text-small text-dark1">{t('p-como-funciona')}</Text>
+        <Text className="text-base font-bold text-black">{t('h2-como-funciona')}</Text>
+        <Text className="text-small text-black">{t('p-como-funciona')}</Text>
         <View className="my-3 gap-2">
-          <StepComoFunciona step="01" description="Cadastre-se facilmente via redes sociais" />
-          <StepComoFunciona
-            step="02"
-            description="Encontre clientes e agende os serviços.
-"
-          />
-          <StepComoFunciona
-            step="03"
-            description="No dia agendado, vá ao local do serviço e faça o check-in."
-          />
-          <StepComoFunciona
-            step="04"
-            description="Após o serviço realize o checkout, e receba em até 3 horas."
-          />
+          <StepComoFunciona step="01">
+            <Text className="text-small leading-snug flex-1 text-black">
+              Cadastre-se facilmente via redes sociais.
+            </Text>
+          </StepComoFunciona>
+          <StepComoFunciona step="02">
+            <Text className="text-small leading-snug flex-1 text-black">
+              Encontre clientes e agende os serviços.
+            </Text>
+          </StepComoFunciona>
+          <StepComoFunciona step="03">
+            <Text className="text-small leading-snug flex-1 text-black">
+              No dia agendado, vá ao local do serviço e faça o check-in.
+            </Text>
+          </StepComoFunciona>
+          <StepComoFunciona step="04">
+            <Text className="text-small leading-snug flex-1 text-black">
+              Após o serviço realize o checkout, e{' '}
+              <Text className="font-bold">receba em até 3 horas.</Text>
+            </Text>
+          </StepComoFunciona>
         </View>
       </View>
     </View>
