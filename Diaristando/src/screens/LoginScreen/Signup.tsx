dbb3c02a3979509baab1e613af013e3cc0c111cc
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2024-09-09 21:04:13 -0400

    Update Signup.tsx

--- Diaristando/src/screens/LoginScreen/Signup.tsx
+++ Diaristando/src/screens/LoginScreen/Signup.tsx
@@ -3,11 +3,10 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 
 import { FocusAwareStatusBar } from '@/components/FocusAwareStatusBar';
-//import BankInfo from '@/components/StepSignup/BankInfo';
 import { PersonalInfo } from '@/components/StepSignup/PersonalInfo';
 import { StepIndicator } from '@/components/StepsIndicator/Step/StepIndicator';
 
-export function Signup() {
+export default function Signup() {
   const [currentStep, setCurrentStep] = useState(1);
   const navigation = useNavigation();
 
@@ -15,7 +14,7 @@ export function Signup() {
     if (currentStep === 1) {
       setCurrentStep(2);
     } else {
-      navigation.navigate('Home');
+      navigation.navigate('PersonalInfo' as never);
       console.log('Complete!');
     }
   };
@@ -23,12 +22,8 @@ export function Signup() {
   return (
     <View className="flex-1">
       <FocusAwareStatusBar backgroundColor="#ffffff" />
-      {/* Usamos o componente StepIndicator com 2 etapas */}
       <StepIndicator currentStep={currentStep} totalSteps={1} />
-
-      {/* Exibe o componente de Informações Pessoais ou Dados Bancários */}
       {currentStep === 1 && <PersonalInfo onNext={handleNextStep} />}
-
     </View>
   );
 }
