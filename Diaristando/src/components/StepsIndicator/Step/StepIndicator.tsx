99563bd726bbfe05af8a464e70ea413bcc64cde8
Nicalo Ribeiro <nicalo63@gmail.com>
2024-09-12 14:47:10 -0300

    refacor: component changed from class to function

--- Diaristando/src/components/StepsIndicator/Step/StepIndicator.tsx
+++ Diaristando/src/components/StepsIndicator/Step/StepIndicator.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export class StepIndicator extends React.Component<{ currentStep: any; totalSteps: any }> {
-  render() {
-    const { currentStep, totalSteps } = this.props;
-    const steps = Array.from({ length: totalSteps }, (_, index) => index + 1);
+type StepIndicatorProps = {
+  currentStep: number;
+  totalSteps: number;
+};
 
-    return (
-      <View className="flex-row items-center justify-between p-3 mb-4 text-small">
-        {steps.map((step) => (
-          <View key={step} className="items-center flex-1">
-            <Text
-              className={`text-lg font-bold ${currentStep === step ? 'text-primary' : 'text-gray'}`}
-            >
-              {`Etapa ${step}`}
-            </Text>
-            <View
-              className={`h-2 rounded-full w-40 mt-2 ${
-                currentStep === step ? 'bg-primary' : 'bg-gray'
-              }`}
-            />
-          </View>
-        ))}
-      </View>
-    );
-  }
+export function StepIndicator({ currentStep, totalSteps }: StepIndicatorProps) {
+  const steps = Array.from({ length: totalSteps }, (_, index) => index + 1);
+
+  return (
+    <View className="flex-row items-center justify-between p-3 mb-4 text-small">
+      {steps.map((step) => (
+        <View key={step} className="items-center flex-1">
+          <Text
+            className={`text-lg font-bold ${currentStep === step ? 'text-primary' : 'text-gray'}`}
+          >
+            {`Etapa ${step}`}
+          </Text>
+          <View
+            className={`h-2 rounded-full w-40 mt-2 ${
+              currentStep === step ? 'bg-primary' : 'bg-gray'
+            }`}
+          />
+        </View>
+      ))}
+    </View>
+  );
 }
