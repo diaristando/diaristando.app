219898add3db63bf2694bc93cff696fb29fbd82a
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2024-10-08 20:19:29 -0400

    Update clienteNavigation.tsx for Codacy

--- Diaristando/src/navigation/clienteNavigation.tsx
+++ Diaristando/src/navigation/clienteNavigation.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
 import { Home } from '@/screens/HomeScreen/Home';
 
 const ClienteNavigation = createNativeStackNavigator();
