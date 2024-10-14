3444b9dc5ade75f570f9f3d3ffecc5db79ae4781
Nicalo Ribeiro <nicalo63@gmail.com>
2024-10-14 18:48:44 -0300

    feat: confirm modal added to page

--- Diaristando/src/screens/ProfileScreen/Profile.tsx
+++ Diaristando/src/screens/ProfileScreen/Profile.tsx
@@ -1,8 +1,10 @@
 import { useUser } from '@clerk/clerk-expo';
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import { Button } from '@/components/Button';
+import { CustomModal } from '@/components/Modal';
 import ProfilePic from '@/components/ProfilePic';
 import { Separator } from '@/components/Separator';
 import { RootState } from '@/store';
@@ -11,12 +13,42 @@ const UserProfile = () => {
   const user = useSelector((state: RootState) => state.user);
   const { user: googleUser } = useUser();
 
+  const [confirmModal, setConfirmModal] = useState(true);
+
   if (!user) {
     return <Text>Carregando...</Text>;
   }
 
   return (
     <View style={styles.container}>
+      <CustomModal
+        isOpen={confirmModal}
+        onClose={() => {
+          setConfirmModal(false);
+        }}
+      >
+        <View
+          style={{ justifyContent: 'center', alignItems: 'center', gap: 12, paddingHorizontal: 36 }}
+        >
+          <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 14 }}>
+            Ótimo! Você estará em contato com clientes que buscam seus serviços
+          </Text>
+          <Text style={{ textAlign: 'center', fontSize: 14 }}>
+            Clique em 'Continuar' para ser direcionada ao WhatsApp.
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'space-between',
+              marginTop: 10,
+            }}
+          >
+            <Button text="Cancelar" onPress={() => setConfirmModal(false)} reverse />
+            <Button text="Continuar" onPress={() => setConfirmModal(false)} />
+          </View>
+        </View>
+      </CustomModal>
       <View style={styles.header}>
         <View style={styles.profilePicContainer}>
           <ProfilePic
@@ -80,7 +112,7 @@ const UserProfile = () => {
         <Text style={styles.mapText}>Futuro mapa</Text>
       </View>
 
-      <TouchableOpacity style={styles.whatsappButton}>
+      <TouchableOpacity style={styles.whatsappButton} onPress={() => setConfirmModal(true)}>
         <Text style={styles.whatsappButtonText}>Contato via WhatsApp</Text>
       </TouchableOpacity>
     </View>
