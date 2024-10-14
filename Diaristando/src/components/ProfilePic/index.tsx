1c9bb8263d7b0a3a8c13fe3aedc716cdd174b567
Nicalo Ribeiro <nicalo63@gmail.com>
2024-10-14 17:50:09 -0300

    fix: profile pic props adjustments

--- Diaristando/src/components/ProfilePic/index.tsx
+++ Diaristando/src/components/ProfilePic/index.tsx
@@ -5,12 +5,20 @@ type ProfilePicProps = {
   imageUrl: string;
   isEditable: boolean;
   handleUpload: () => void;
+  height?: number;
+  width?: number;
 };
 
-const ProfilePic = ({ imageUrl, isEditable, handleUpload }: ProfilePicProps) => {
+const ProfilePic = ({
+  imageUrl,
+  isEditable,
+  handleUpload,
+  height = 100,
+  width = 100,
+}: ProfilePicProps) => {
   return (
-    <View style={{ borderRadius: 50, overflow: 'hidden' }}>
-      <Image source={{ uri: imageUrl }} style={{ width: 100, height: 100 }} />
+    <View style={{ borderRadius: 50, overflow: 'hidden', backgroundColor: 'red' }}>
+      <Image source={{ uri: imageUrl }} style={{ width, height }} />
       {isEditable && (
         <TouchableOpacity onPress={handleUpload}>
           <Text style={{ color: 'blue' }}>Editar Foto</Text>
