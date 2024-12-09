53682a29820545c6122ddc184d92ffee271b6f81
Nicalo Ribeiro <nicalo63@gmail.com>
2024-12-09 19:57:11 -0300

    fix: lint fixes

--- Diaristando/src/screens/HomeScreen/Home.tsx
+++ Diaristando/src/screens/HomeScreen/Home.tsx
@@ -28,23 +28,23 @@ export function Home() {
                         <View style={styles.stepsContainer}>
                             <StepsCard step="01">
                                 <Text style={styles.stepText}>
-                                    Cadastre-se facilmente via redes sociais.
+                                    Cadastre-se facilmente via rede social.
                                 </Text>
                             </StepsCard>
                             <StepsCard step="02">
                                 <Text style={styles.stepText}>
-                                    Encontre clientes e agende os serviços.
+                                    Encontre clientes ou diaristas de acordo com sua localização.
                                 </Text>
                             </StepsCard>
                             <StepsCard step="03">
                                 <Text style={styles.stepText}>
-                                    No dia agendado, vá ao local do serviço e faça o check-in.
+                                    Registre os serviços no app e gerencie o agendamento pelo
+                                    aplicativo.
                                 </Text>
                             </StepsCard>
                             <StepsCard step="04">
                                 <Text style={styles.stepText}>
-                                    Após o serviço realize o checkout, e{' '}
-                                    <Text style={styles.boldText}>receba em até 3 horas.</Text>
+                                    Forneça o feedback sobre o serviço.
                                 </Text>
                             </StepsCard>
                         </View>
@@ -105,8 +105,5 @@ const styles = StyleSheet.create({
         lineHeight: 18.2,
         flex: 1,
         color: '#000000',
-    },
-    boldText: {
-        fontWeight: 'bold',
     },
 });
